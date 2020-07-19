d8bb862ec64c036e65c116254ea44ddb76b9a1c1
ahmetikrdg <karadagahmetkaa@outlook.com>
2020-07-19 16:51:56 +0300

    first commit

---
+++ Migrations/20200719133921_InitialCreate.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProdutId = table.Column<int>(nullable: false),
+                    DateAdded = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    InsertedDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LastUpdatedDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(maxLength: 15, nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategory",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategory", x => new { x.ProductId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Fullname = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Customer_id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdentityNumber = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Customer_id);
+                    table.ForeignKey(
+                        name: "FK_Customer_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_UserId",
+                table: "Customer",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_CategoryId",
+                table: "ProductCategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategory");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFD
+{
+
+    public class ShopContext : DbContext
+    {
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });//entity komutum sqlde nasıl göremk için ekledim
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory).UseMySql(@"server=localhost;port=3306;database=ShopDb;user=root;password=1532blmz");
+
+            // //entity komutum sqlde nasıl göremk için ekledim bu satırı
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()//customer tablosuna konumlandım
+                   .Property(b => b.Id)//property özelliğilye ilgili elemana gidiyoruz
+                   .IsRequired();//diyerek zorunlu alan olarak tanımladık
+
+            modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)//userin hangi alanda ona index ekle
+            .IsUnique();//ve bu benzersiz olmalı
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasKey(bc => new { bc.ProductId, bc.CategoryId });
+            modelBuilder.Entity<ProductCategory>()
+                .HasOne(bc => bc.product)
+                .WithMany(b => b.ProductCategories)
+                .HasForeignKey(bc => bc.ProductId);
+            modelBuilder.Entity<ProductCategory>()
+                .HasOne(bc => bc.category)
+                .WithMany(c => c.ProductCategories)
+                .HasForeignKey(bc => bc.CategoryId);
+        }
+    }
+    public class User
+    {//one to many bir kullanıcının birden fazla adresi olabilir
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(15), MinLength(10)]
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<Address> Addresses { get; set; }//elde ettiğim herhangi userin üzerinden adresses dediğim zaman adres bilgisi gelecek.bir userin birde nfazla adresi olabileceği için list
+                                                    //herhang bir kullanıcı üzerinden addreses dersem o kullanıcının adersi gelir
+                                                    //bir kullanıcının birden fazla adresi olabilir. Birkaç tane adres yanlızca bir usere ait olmalı.
+    }
+
+    public class Customer
+    {
+        [Column("Customer_id")]//uygulamadaki Id Customer_id olur 
+        public int Id { get; set; }
+        public string IdentityNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]//tabloda gözükmesin
+        public string FullName { get; set; }
+        public User User { get; set; }//iki tablodada böyle tanımladım list tanımlasaydım userde bire çok olurdu.
+        public int UserId { get; set; }//neden userıd customerda
+        //bir user kaydını database eklemem gerekir ve bu userin oluşturulan ıd bilgisini alıp o id bilgisiyle customer kaydı oluşturacaksın.Ancak oluşturulan user ıd bilgisini işte bu tabloya eklemen lazım.
+    }
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public User User { get; set; }//user objesi oluşturdum.adresler tablosundaki her kayıt tek bir usere ait.bir user birine ait o yüzden user yukarıda list çünkü birden fazla adresi olabilir dedik
+        public int UserId { get; set; }//eklemiş olduğum herhangi bir userin ıd bilgisini kullanarak gelip adres tablosuna bir kayırt ekleyebilirim
+        //murlaka bir userin ıdsi var demek int? yaparsan nulda olsa sıkıntı olmaz//yabancı anahtar bu user
+    }
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]//artık Id kolonu kendi kendine artmaz bizim yazmamız gerekir
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//kayıt oluştururken kayıt tarihini alır 
+        public DateTime InsertedDate { get; set; } = DateTime.Now;
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastUpdatedDate { get; set; }
+
+        public int CategoryId { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+    public class ProductCategory
+    {
+        public int ProductId { get; set; }
+        public Product product { get; set; }
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProdutId { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var DB = new ShopContext())
+            {
+                var p=new Product(){
+                    Name="Samsung s6",
+                    Price=2000
+
+                };
+                DB.products.Add(p);
+                DB.SaveChanges();
+
+            }
+        }
+        static void InsertUsers()
+        {
+            var users = new List<User>(){
+                new User(){Username="Ahmet Karadağ",Email="ahmetikrdg@outlook.com"},
+                new User(){Username="Yiğit Bilge",Email="yigitbilge@outlook.com"},
+                new User(){Username="Ali Çelik",Email="celikali@outlook.com"},
+                new User(){Username="Mehmet Güz",Email="cmet@outlook.com"}
+            };
+            using (var db = new ShopContext())
+            {
+                db.Users.AddRange(users);
+                db.SaveChanges();
+            }
+        }
+        static void InsertAddresess()
+        {
+            var Adreses = new List<Address>(){
+                new Address(){Fullname="Ahmet Karadağ",Title="Ev Adresi",Body="İstanbul",UserId=1},
+                new Address(){Fullname="Yiğit Bilge",Title="İş Adresi",Body="İstanbul",UserId=2},
+                new Address(){Fullname="Ali Çelik",Title="Ev Adresi",Body="İstanbul",UserId=3},
+                                new Address(){Fullname="Ali Çelik",Title="İş Adresi",Body="İstanbul",UserId=3},
+                                new Address(){Fullname="Mehmet Güz",Title="İş Adresi",Body="İstanbul",UserId=4},
+                                new Address(){Fullname="Mehmet Güz",Title="Ev Adresi",Body="İstanbul",UserId=4}
+            };
+            using (var db = new ShopContext())
+            {
+                db.Addresses.AddRange(Adreses);
+                db.SaveChanges();
+            }
+        }
+    }
+    /*
+        [NotMapped]//database içerisinde bir tablo olarak oluşturulmaz
+        veya tablo ismi şöyle olsun 
+        [Table("UrunKategorileri")]
+        yada bu tablo adı değiştiremnin fluentli hali
+        modelBuilder.Entity<Car>().ToTable("Urunler");
+        class Car
+        {
+            [Key] //LicensePlate id olsun dedim anlaması için key dedim
+            public string LicensePlate { get; set; }
+
+            public string Make { get; set; }
+            public string Model { get; set; }
+            ----------------
+             fluent apili hali shopcontextin içinde olmalı.
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Car>()
+                    .HasKey(c => c.LicensePlate);
+            }*/
+}
+
+
+
+
+
+
+
+
